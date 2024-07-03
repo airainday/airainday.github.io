e0d5c9679bf024ab2b6fac4f8d044cbfbfa16837
airainday <68547596+airainday@users.noreply.github.com>
2024-07-04 07:19:29 +0800

    Site updated: 2024-07-04 07:19:28

---
+++ js/backgroundize.js
@@ -0,0 +1,11 @@
+document
+  .querySelector('#web_bg')
+  .setAttribute('style', `position: fixed;width: 100%;height: 100%;z-index: -1;background-size: cover;`);
+  
+document
+  .querySelector("#banner")
+  .setAttribute('style', 'background-image: url()')
+
+document
+  .querySelector("#banner .mask")
+  .setAttribute('style', 'background-color:rgba(0,0,0,0)')
