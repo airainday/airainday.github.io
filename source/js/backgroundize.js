37e5f31aa50eaae11505795a957fedfc104525f1
airainday <68547596+airainday@users.noreply.github.com>
2024-07-04 07:19:51 +0800

    1

--- source/js/backgroundize.js
+++ source/js/backgroundize.js
@@ -1,7 +1,7 @@
 document
   .querySelector('#web_bg')
-  .setAttribute('style', `background-image: ${document.querySelector('.banner').style.background.split(' ')[0]};position: fixed;width: 100%;height: 100%;z-index: -1;background-size: cover;`);
-
+  .setAttribute('style', `position: fixed;width: 100%;height: 100%;z-index: -1;background-size: cover;`);
+  
 document
   .querySelector("#banner")
   .setAttribute('style', 'background-image: url()')
